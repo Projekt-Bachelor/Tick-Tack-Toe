a40d1993d972a8ec357b57ff14dba49df5ed4c6c
marie-mops <38949215+marie-mops@users.noreply.github.com>
2018-06-01 12:32:48 +0200

    Update Stein.java

--- src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/model/Stein.java
+++ src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/model/Stein.java
@@ -3,7 +3,7 @@ package de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model;
 public enum Stein {
 
         // Spielsteine erzeugen vom Typ Stein
-        Kreis, // soll 0 sein,
-        Kreuz,  //soll 1 sein
+        Kreis, // boolean als Alternative dann 0
+        Kreuz,  //dann 1
 
 }
