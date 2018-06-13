e859906ef94ed2c369c11d531e9e39a3658649b2
marie-mops <mgo16@tu-clausthal.de>
2018-06-13 12:51:09 +0200

    Changed .gitignore to add css files, added css files and modified index.html

--- src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/CApplication.java
+++ src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/CApplication.java
@@ -1,25 +1,43 @@
 package de.tu_clausthal.in.bachelorproject2018.ticktacktoe;
 
+import de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model.Spiel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model.Spiel.*;
 
 /**
  * Applikation
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-public class CApplication
-{
-
+public class CApplication {
     /**
      * main
      *
      * @param p_args arguments
      */
-    public static void main( final String[] p_args )
-    {
-        SpringApplication.run( CApplication.class, p_args );
+    public static void main(final String[] p_args) {
+        SpringApplication.run(CApplication.class, p_args);
+        if(Runde == false){
+            //neuesSpiel();
+            neuesFeld();
+            Anfaenger = true;
+        }
+        else {
+            System.out.println("Es wurde kein Spielstein gesetzt! Setzten sie bitte zum Vortfahren einen Spielstein");
+            int x = -1;
+            int y=-1;
+            Spiel.setzen(x, y);// Benutzer setzt einen Stein
+        }
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(@ModelAttribute("spiel") Spiel spiel) {
+        return "index";
     }
 
 }
--- src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/model/Spiel.java
+++ src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/model/Spiel.java
@@ -1,19 +1,21 @@
 package de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Arrays;
 
-import static de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model.Spielbrett.*;
+import static de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model.Spiel.neuesSpiel;
+import static de.tu_clausthal.in.bachelorproject2018.ticktacktoe.model.Stein.Kreis;
 
 public class Spiel {
-
-    boolean Anfaenger;
-    Stein[][] aktuellesFeld = Spielbrett.feld;
+    public static boolean Runde = false;
+    public static boolean Anfaenger;
+    static Stein[][] aktuellesFeld = Spielbrett.feld;
     //int size = Spielbrett.check();
     int size = 3;
 
+
     boolean anfaenger() {
+        //0 ist der Kreis und 1 ist X
         Random rand = new Random();
         Anfaenger = rand.nextBoolean();
         ;
@@ -21,12 +23,22 @@ public class Spiel {
 
     }
 
-    boolean setzen() {
-        if (Spielbrett.leeresFeld == true) {
-            neuesFeld();
-            return false;
-        } else return true;
+    public static void neuesSpiel(){
+        Runde=true;
+        neuesFeld();
+    }
 
+    public static void setzen(int x, int y) {
+        if(Runde == false){
+            neuesSpiel();
+        }
+        else {
+            System.out.println("Es wurde kein Spielstein gesetzt! Setzten sie bitte zum Vortfahren einen Spielstein");
+            //int x = -1;
+            //int y=-1;
+            Spiel.setzen(x, y);// Benutzer setzt einen Stein
+        }
+        aktuellesFeld[x][y]=Kreis;
     }
 
     /* Alle Felder gefüllt */
@@ -36,7 +48,7 @@ public class Spiel {
 
 
     /* falls ein neues Spiel erstellt werden soll oder der Reset Botton verwendet wird */
-    void neuesFeld() {
+    public static void neuesFeld() {
         Arrays.fill(Spielbrett.feld, null);
 
     }
@@ -49,7 +61,7 @@ public class Spiel {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
 
-                if (aktuellesFeld[i][j] == Stein.Kreis) OCounter++;
+                if (aktuellesFeld[i][j] == Kreis) OCounter++;
                 else if (aktuellesFeld[i][j] == Stein.Kreuz) {
                     XCounter++;
                 }
@@ -62,7 +74,7 @@ public class Spiel {
 
         /*Diagonal links prüfen*/
         for (int i = size - 1, j = size - 1; i >= 0; i--, j--) {
-            if (aktuellesFeld[i][j] == Stein.Kreis) OCounter++;
+            if (aktuellesFeld[i][j] == Kreis) OCounter++;
             else if (aktuellesFeld[i][j] == Stein.Kreuz) XCounter++;
         }
         if (OCounter == size || XCounter == size) return true;
@@ -70,7 +82,7 @@ public class Spiel {
         /*Diagonal rechts prüfen */
 
         for (int i = 0, j = 0; i < size; i++, j++) {
-            if (aktuellesFeld[i][j] == Stein.Kreis) OCounter++;
+            if (aktuellesFeld[i][j] == Kreis) OCounter++;
             else if (aktuellesFeld[i][j] == Stein.Kreuz) XCounter++;
         }
         if (OCounter == size || XCounter == size) return true;
