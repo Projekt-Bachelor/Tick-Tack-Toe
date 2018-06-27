681e3cf502592924b16fcd60c301f3bbd9976661
Unknown <tim.roethig@yahoo.de>
2018-06-27 15:08:50 +0200

    fix set return

--- src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/controller/CSetReturn.java
+++ src/main/java/de/tu_clausthal/in/bachelorproject2018/ticktacktoe/controller/CSetReturn.java
@@ -6,13 +6,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class CSetReturn {
 
-    private final AtomicReference<IItem>[][] elements;
     private final boolean won;
     private final boolean draw;
-
-    public AtomicReference<IItem>[][] getElements() {
-        return elements;
-    }
 
     public boolean isWon() {
         return won;
@@ -22,9 +17,8 @@ public class CSetReturn {
         return draw;
     }
 
-    public CSetReturn(AtomicReference<IItem>[][] elements, boolean won, boolean draw){
+    public CSetReturn (boolean won, boolean draw){
 
-        this.elements = elements;
         this.won = won;
         this.draw = draw;
     }
